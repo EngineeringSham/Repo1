49e1b9919fb71be20a6bdddbe8d55490f59e12d9
Amol Garje <atulgarje222@gmail.com>
2024-02-02 16:02:20 +0530

    third-commit

--- Amol.java
+++ Amol.java
@@ -5,6 +5,10 @@ class Amol{
         System.out.println("Addition Of Your Two Number is "+20);
     }
 
+    public static void Substraction(){
+        System.out.println("Addition Of Your Two Number is "+20);
+    }
+
     public static void main(String[] args) {
              Amol.DoAddition();
              System.out.println("Your Substraction Functnality We Used In the After Given Line....");
