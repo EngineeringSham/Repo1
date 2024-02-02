be92e2faf7343aab74333ee76262192efca1fdd7
Amol Garje <atulgarje222@gmail.com>
2024-02-02 15:13:00 +0530

    First-Commite

---
+++ Amol.java
@@ -0,0 +1,11 @@
+class Amol{
+
+    // My Addition file Here....
+    public static void DoAddition(){
+        System.out.println("Addition Of Your Two Number is "+20);
+    }
+
+    public static void main(String[] args) {
+             Amol.DoAddition();
+    }
+}
